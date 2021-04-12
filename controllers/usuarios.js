a4d357e609ddc321c1cf142197f02da916ca6146
Jose Bedoya <jose.bedoya@gmail.com>
2021-04-12 07:20:14 -0500

    Primer commit 1.1

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,10 @@
-const {response} = require('express')
+const {response, request} = require('express')
+const { validationResult } = require('express-validator')
+const bcryptjs = require('bcryptjs')
 
+const Usuario = require('../models/usuario')
 
-const usuariosGet = (req, res= response) => {
+const usuariosGet = (req = request, res= response) => {
     const {q, nombre='sin nombre', apikey,pag=1,limit} = req.query
     res.json({
         msg: 'get API -controlador',
@@ -9,13 +12,36 @@ const usuariosGet = (req, res= response) => {
     })
 }
 
-const usuariosPost = (req, res= response) => {
-    const {nombre, edad} = req.body
+const usuariosPost = async (req, res= response) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json( errors )
+    }
+    const {nombre, correo, clave, rol} = req.body
+    const usuario = new Usuario( {nombre, correo, clave, rol} )
+
+    //verificar si existe correo
+    const existeCorreo = await Usuario.findOne({correo})
+    if (existeCorreo) {
+        return res.status(404).json({
+            msg: 'Ese correo ya esta registrado'
+        })
+    }
+
+    //encriptar la clave
+    const salt = bcryptjs.genSaltSync();
+    usuario.clave = bcryptjs.hashSync(clave, salt)
+
+    //guardar en DB
+    await usuario.save()
+
     res.json({
-        msg: 'post API -controlador',
-        nombre,edad
+        msg: 'post API -controlador local',
+        usuario
     })
 }
+
 const usuariosPut = (req, res= response) => {
     const { id } = req.params
     res.json({
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,13 @@
-
 const { Router } = require('express')
+const { check } = require('express-validator')
 const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require('../controllers/usuarios')
 
 const router = Router()
 
 router.get('/', usuariosGet )
-router.post('/', usuariosPost)
+router.post('/', [
+    check('correo', 'El correo no es valido').isEmail(),
+], usuariosPost)
 router.put('/:id', usuariosPut)
 router.delete('/', usuariosDelete)
 router.patch('/', usuariosPatch)
